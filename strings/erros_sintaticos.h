736760b87205c6f8bc6b2af0dae52e9394719f85
gabriel-76 <gabrielrodrigues_76@computacao.ufla.br>
2016-07-02 10:28:12 -0300

    Arquivo de strings foi criado

---
+++ strings/erros_sintaticos.h
@@ -0,0 +1,9 @@
+#ifndef ERROS_SINTATICOS_H
+#define ERROS_SINTATICOS_H
+
+#inclide <iostream>
+
+#define ERRO_PONTO_E_VIRGULA "Era esperado um ponto e virgula ao final da frase"
+
+
+#endif /*ERROS_SINTATICOS_H*/
